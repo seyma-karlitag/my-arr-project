865339fda6578f05062726c80737f3e307bf98de
seyma-karlitag <seymakarlitag39@gmail.com>
2025-11-03 15:17:18 +0300

    Remove unnecessary Unity-generated folders

--- Library/PackageCache/com.unity.mobile.android-logcat@1552651bf770/Editor/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.Integration.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.Mobile.AndroidLogcat.Performance.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.Mobile.DeviceExplorer.Editor")]
